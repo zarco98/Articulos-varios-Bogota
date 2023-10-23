3243b131a922a4befd5e88cab02e0d1ba1cac523
Felipe Fonseca <kf.fonseca10@uniandes.edu.co>
2023-10-23 12:00:35 -0500

    Clase 4

--- back/server.js
+++ back/server.js
@@ -2,9 +2,12 @@ const app=require("./app")
 
 //Setear el archivo de configuración
 const dotenv=require("dotenv");
+const connectDatabase = require("./config/database");
 dotenv.config({path: 'back/config/config.env'})
+
+//Configurar base de datos
+connectDatabase();
 
 
 const server=app.listen(process.env.PORT, () => {
-    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
-})
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)})
